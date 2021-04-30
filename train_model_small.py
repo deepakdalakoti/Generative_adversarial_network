e3a20b60e174cfae4f39417a2ddd6f0dbf876055
Deepak Dalakoti <deepakkdalakoti@gmail.com>
2021-04-30 11:36:12 +1000

    Training script

--- train_model_small.py
+++ train_model_small.py
@@ -1,9 +1,8 @@
 from model.PIESRGAN import PIESRGAN
 from utils.util import DataLoader_s3d, do_normalisation, Image_generator2, do_inverse_normalisation
-from utils.util import Image_generator
+from utils.util import Image_generator, write_all_wghts, tensorboard_stats
 import time
 import tensorflow as tf
-import horovod.tensorflow.keras as hvd
 import numpy as np
 from utils.gan_post import GAN_post
 from tensorflow.keras.callbacks import CSVLogger, ReduceLROnPlateau
@@ -47,8 +46,8 @@ def save_model_generator(model, savedir, idx):
 
 def load_model_generator(model,savedir, idx):
     print("loading model from epoch {}".format(idx))
-    model.generator.load_weights(savedir+'generator_idx_{}.h5'.format(idx))
-    opt_wght = pickle.load(open(savedir+'generator_opt_idx_{}.h5'.format(idx),'rb'))
+    model.generator.load_weights(savedir+'test_{}.h5'.format(idx))
+    opt_wght = pickle.load(open(savedir+'test_opt_idx_{}.h5'.format(idx),'rb'))
     def load_opt():
         grad_vars = model.generator.trainable_weights
         zero_grads = [tf.zeros_like(w) for w in grad_vars]
@@ -91,59 +90,63 @@ def load_model_gan(model,savedir, idx, Gen=True, Dis=False):
 
     return
 
-hvd.init()
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
-if gpus:
-    tf.config.experimental.set_visible_devices(gpus[hvd.local_rank()], 'GPU')
-
-opt = tf.keras.optimizers.Adam(learning_rate=5e-4)
-distopt = hvd.DistributedOptimizer(opt)
 nxg, nyg, nzg = 384, 384, 384
-batch_size=96
+batch_size=128
 boxsize=32
 upsc = 4
-#del udns
-#del ufilt
+epochs=50000000
+print_frequency = 200
+save_frequency = 500
+fig_frequency = 50
+savedir = './data/'
+#mirrored_strategy = tf.distribute.MirroredStrategy()
+mode = 0 # 1 = GAN, 0 = generator
+datapath_train='/scratch/w47/share/IsotropicTurb'
+train_loader_hr = DataLoader_s3d(datapath_train+'/DNS_Relambda_162_up_50_Lt_2mm/s-7.0000E-06', nxg*upsc, nyg*upsc, nzg*upsc, 2, batch_size, boxsize*upsc, 1)
+train_loader_hr.get_norm_constants(48)
+train_loader_lr=GAN_post(datapath_train+'/Filtered_Relambda_162_up_50_Lt_2mm/Filt_4x_regrid_384/s-7.0000E-06', nxg, nyg, nzg, 2, batch_size, boxsize, 1, 1)
+norm='std'
+mins = train_loader_hr.mean
+maxs = train_loader_hr.std
+
+
+
+udns = train_loader_hr.read_parallel(48)
+ufilt = train_loader_lr.read_parallel(48)
+udns = train_loader_hr.reshape_array([boxsize*upsc, boxsize*upsc, boxsize*upsc], udns)
+ufilt = train_loader_lr.reshape_array([boxsize, boxsize, boxsize], ufilt)
+
+
 if(mode==0):
     ''' Use keras fit API here, seems faster than custom training loop'''
  
     gan = PIESRGAN(training_mode=True,
                     height_lr = boxsize, width_lr=boxsize, depth_lr=boxsize,
-                    gen_lr=5.0e-4, dis_lr=2e-6,
+                    gen_lr=6.0e-4, dis_lr=2e-6,
                     channels=1, RRDB_layers=3,
                     upscaling_factor=upsc,
-                    opt = distopt
                     )
-    gan.batch_size=batch_size
-    callbacks = [
-    # Horovod: broadcast initial variable states from rank 0 to all other processes.
-    # This is necessary to ensure consistent initialization of all workers when
-    # training is started with random weights or restored from a checkpoint.
-    hvd.callbacks.BroadcastGlobalVariablesCallback(0),
-    ] 
-    #load_model_generator(gan, savedir, 0)
+    load_model_generator(gan, savedir, 350)
     # callbacks
     #log_dir = "./logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    #tensorboard_callback = tensorboard_stats(50, './logs/', 0)
-    #csv_logger = CSVLogger("upsamp.csv", append=True)
-    #reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.95, patience=5, min_lr=0.00001,verbose=1,mode='min')
-    #save_wghts = write_all_wghts(100, './data/', 'test', 0)
+    tensorboard_callback = tensorboard_stats(1, './logs/', 0)
+    csv_logger = CSVLogger("upsamp.csv", append=True)
+    reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.95, patience=5, min_lr=0.00005,verbose=1,mode='min')
+    save_wghts = write_all_wghts(5, './data/', 'test', 0)
 
     idx=0
 
-    rands = np.arange(udns.shape[0])
-    np.random.shuffle(rands)
-    udns = udns[rands,:,:,:,:]
-    ufilt = ufilt[rands,:,:,:,:]
+    #rands = np.arange(udns.shape[0])
+    #np.random.shuffle(rands)
+    #udns = udns[rands,:,:,:,:]
+    #ufilt = ufilt[rands,:,:,:,:]
     ufilt = do_normalisation(ufilt,norm, mins, maxs)
     udns = do_normalisation(udns,norm, mins, maxs)
         
     for i in range(0,epochs):
  
-        imgs_hr = udns[idx*batch_size:(idx+1)*batch_size, :,:,:,:]
-        imgs_lr = ufilt[idx*batch_size:(idx+1)*batch_size,:,:,:,:]
+        #imgs_hr = udns[idx*batch_size:(idx+1)*batch_size, :,:,:,:]
+        #imgs_lr = ufilt[idx*batch_size:(idx+1)*batch_size,:,:,:,:]
 
         idx=idx+1
         if(idx>train_loader_lr.nbatches-1):
@@ -151,13 +154,12 @@ if(mode==0):
 
         #t1=time.time()
         gan.generator.fit(
-                    imgs_lr, imgs_hr,
-                    batch_size=8,
-                    epochs=10,
-                    callbacks = callbacks,
-                    verbose=1 if hvd.rank() == 0 else 0
+                    ufilt, udns,
+                    batch_size=32,
+                    epochs=1000,
+                    #callbacks = callbacks,
                     #callbacks = [csv_logger, tensorboard_callback, reduce_lr, check],
-                    #callbacks =  [csv_logger, save_wghts, tensorboard_callback, reduce_lr]
+                    callbacks =  [csv_logger, save_wghts, tensorboard_callback, reduce_lr]
                     )
         #print("TIME {}".format(time.time()-t1))
 
